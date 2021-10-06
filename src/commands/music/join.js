51b9d96aee7a947b27076b920bc5e0d3c2844fd1
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-10-06 06:08:08 +0530

    🐼AnnieSIS

---
+++ src/commands/music/join.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('Play a song!'),
+
+  async run (msg) {
+    const voiceChannel = msg.member.voice.channel
+    if (!voiceChannel) return msg.reply('Please enter a voice channel first.')
+    if (!voiceChannel.permissionsFor(this.client.user).has(['CONNECT', 'SPEAK', 'VIEW_CHANNEL'])) {
+      return msg.reply('I\'m missing the "Connect", "Speak", or "View Channel" permission for this channel.')
+    }
+    if (!voiceChannel.joinable) return msg.reply('Your voice channel is not joinable.')
+    if (this.client.voice.connections.has(voiceChannel.guild.id)) {
+      return msg.reply('I am already in a voice channel.')
+    }
+    await voiceChannel.join()
+    return msg.reply(`Joined **${voiceChannel.name}**!`)
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,56 @@
+const { Client, Collection, Intents } = require('discord.js');
+const fs = require('fs');
+const Levels = require('discord-xp')
+const path = require('path')
+require('dotenv').config()
+
+Levels.setURL(process.env.mongoPath)
+
+const dotenv = require('dotenv')
+
+dotenv.config({
+  path: path.join(__dirname, '..', '.env')
+})
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.on('message', async (message) => {
+  if (message.author.bot) return
+  if (message.channel.type === 'dm') return
+  const randomXp = Math.floor(Math.random() * 10) + 10
+  const hasLevelUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp)
+  
+  if (hasLevelUp) {
+    message.reply(`Congrats! You've leveled up to level: ${(await Levels.fetch(message.author.id, message.guild.id)).level}`)
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
+
